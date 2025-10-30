f64182f49999f07ba436675e77524579e05ee287
Koen Van Impe <koen.vanimpe@cudeso.be>
2025-10-30 20:53:47 +0100

    Add constants for upload indicators and file name

---
+++ constants.py
@@ -0,0 +1,2 @@
+UPLOAD_INDICATOR_MISP_ACCEPTED_TYPES = ["ip-src", "ip-dst", "ip-src|port", "ip-dst|port", "domain", "hostname", "hostname|port", "url", "sha256", "sha1", "md5"]
+PARSED_INDICATORS_FILE_NAME = "parsed_indicators.json"
